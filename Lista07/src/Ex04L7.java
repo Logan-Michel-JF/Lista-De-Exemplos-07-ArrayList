1f289c246177112a1120e11545c0a66caed88e6b
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-06-08 08:19:41 -0300

    Termino do Exercicio 4 e inicio do 5

--- Lista07/src/Ex04L7.java
+++ Lista07/src/Ex04L7.java
@@ -1,20 +1,26 @@
+
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
-
+ *
  * @author Logan Michel
  */
 public class Ex04L7 {
 
     public static void main(String[] args) {
         ArrayList<Integer> numeros = new ArrayList<>();
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             numeros.add(Integer.parseInt(JOptionPane.showInputDialog("Digite os Números.")));
         }
-        numeros.remove(numeros.get(2));
+        JOptionPane.showMessageDialog(null, "Seus Números: " + numeros); 
+        
         numeros.remove(numeros.get(3));
+        
         JOptionPane.showMessageDialog(null, "Seus Números: " + numeros);
-    
+        
+        numeros.remove(numeros.get(4));
+        
+        JOptionPane.showMessageDialog(null, "Seus Números: " + numeros);
     }
 }
---
+++ Lista07/src/Ex05L7.java
@@ -0,0 +1,13 @@
+
+
+/**
+ 
+ * @author Logan Michel
+ */
+public class Ex05L7 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
